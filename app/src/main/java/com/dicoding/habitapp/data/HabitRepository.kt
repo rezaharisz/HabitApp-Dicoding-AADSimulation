eae25d801a7e9e84d7f481fbfc111c27bb49fbc8
rezaharisz <rezavonstrucker@gmail.com>
2021-07-26 16:52:44 +0700

    done task 1,2,3,4,5

---
+++ app/src/main/java/com/dicoding/habitapp/data/HabitRepository.kt
@@ -0,0 +1,60 @@
+package com.dicoding.habitapp.data
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
+import androidx.paging.toLiveData
+import com.dicoding.habitapp.utils.HabitSortType
+import com.dicoding.habitapp.utils.SortUtils
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class HabitRepository(private val habitDao: HabitDao, private val executor: ExecutorService) {
+
+    companion object {
+
+        const val PAGE_SIZE = 30
+
+        @Volatile
+        private var instance: HabitRepository? = null
+
+        fun getInstance(context: Context): HabitRepository {
+            return instance ?: synchronized(this) {
+                if (instance == null) {
+                    val database = HabitDatabase.getInstance(context)
+                    instance = HabitRepository(
+                        database.habitDao(),
+                        Executors.newSingleThreadExecutor()
+                    )
+                }
+                return instance as HabitRepository
+            }
+
+        }
+    }
+
+    //TODO 4 : Use SortUtils.getSortedQuery to create sortable query and build paged list
+    fun getHabits(filter: HabitSortType): LiveData<PagedList<Habit>> {
+        val sortedQuery = SortUtils.getSorteredQuery(filter)
+        return habitDao.getHabits(sortedQuery).toLiveData(PAGE_SIZE)
+    }
+
+    //TODO 5 : Complete other function inside repository
+    fun getHabitById(habitId: Int): LiveData<Habit> {
+        return habitDao.getHabitById(habitId)
+    }
+
+    fun insertHabit(newHabit: Habit): Long {
+        return habitDao.insertHabit(newHabit)
+    }
+
+    fun deleteHabit(habit: Habit) {
+        executor.execute {
+            habitDao.deleteHabit(habit)
+        }
+    }
+
+    fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit> {
+        throw NotImplementedError("Not yet implemented")
+    }
+}
